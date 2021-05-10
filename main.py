8a207d8e76158b83a71811d37df97903c42aedd8
Ghost <ghostgamesv11@gmail.com>
2021-05-10 12:01:40 +0200

    pandas2

--- main.py
+++ main.py
@@ -12,10 +12,18 @@ import matplotlib.pyplot as plt
 # plt.show()
 
 #zad 2
-imiona2=pd.ExcelFile('imiona.xlsx')
-df2=pd.read_excel(imiona2, header=0)
-grupa=df2.groupby(['Plec']).agg({'Liczba': {'sum'}})
-wykres =grupa.plot.bar()
-wykres.set_ylabel('Mld')
-plt.show()
+# imiona2=pd.ExcelFile('imiona.xlsx')
+# df2=pd.read_excel(imiona2, header=0)
+# grupa=df2.groupby(['Plec']).agg({'Liczba': {'sum'}})
+# wykres =grupa.plot.bar()
+# wykres.set_ylabel('Mld')
+# plt.show()
 
+#zad 3
+imiona3=pd.ExcelFile('imiona.xlsx')
+df3=pd.read_excel(imiona3, header=0)
+grupa = df3.where(df3['Rok'] > 2012).groupby(['Plec']).agg({'Liczba': {'sum'}})
+wykres = grupa.plot.pie(subplots=True,autopct='%.2f %%',fontsize=20,figsize=(6,6),legend=(0,0))
+plt.legend(loc='lower right')
+plt.title('Suma')
+plt.show()
