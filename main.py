15cbcffdf4a876461001623bc8fbd94c01656e48
Ghost <ghostgamesv11@gmail.com>
2021-05-10 11:39:22 +0200

    pandas2

--- main.py
+++ main.py
@@ -3,20 +3,19 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 #zad1
-imiona=pd.ExcelFile("imiona.xlsx")
-df=pd.read_excel(imiona, header=0)
-print(df)
-df1=df.groupby(['Rok'])['Liczba'].sum()
-print(df1)
-df1.plot()
-plt.show()
+# imiona=pd.ExcelFile("imiona.xlsx")
+# df=pd.read_excel(imiona, header=0)
+# print(df)
+# df1=df.groupby(['Rok'])['Liczba'].sum()
+# print(df1)
+# df1.plot()
+# plt.show()
 
 #zad 2
-df2 = df[(df.Plec=='M')]['Liczba'].sum()
-df3 = df[(df.Plec=='K')]['Liczba'].sum()
-e = ['K','M']
-w = [df3, df2]
-plt.bar(e,w)
+imiona2=pd.ExcelFile('imiona.xlsx')
+df2=pd.read_excel(imiona2, header=0)
+grupa=df2.groupby(['Plec']).agg({'Liczba': {'sum'}})
+wykres =grupa.plot.bar()
+wykres.set_ylabel('Mld')
 plt.show()
-
 
